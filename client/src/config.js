69f917ad9187b2ee225bccac7aa75579b7538422
Darya Komsa <komsa.darya@gmail.com>
2018-10-26 19:54:39 -0400

    un-ignored .config for client (no sensitive info there)

---
+++ client/src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  oAuth: {
+    facebook: {
+      clientId: '329783927588448'
+    },
+    google: {
+      clientId: '76763097253-sr9l6reunanr5s82l64ep2io3tbb5ac7.apps.googleusercontent.com'
+    }
+  }
+};
+
+export default config;
